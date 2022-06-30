2fa7eab9a5d99a549b0b8043fd528b75767129aa
John Godley <johng75@gmail.com>
2022-06-30 13:48:26 +0100

    Bump version to 1.3.0

--- gutenberg-everywhere.php
+++ gutenberg-everywhere.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Gutenberg Everywhere
 Description: Because somewhere is just not enough. Add Gutenberg to WordPress comments, bbPress forums, and BuddyPress streams. Also enables Gutenberg for comment & bbPress moderation.
-Version: 1.2.1
+Version: 1.3.0
 Author: Automattic
 Text Domain: 'gutenberg-everywhere'
 */

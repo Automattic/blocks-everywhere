5c5b1fc2c19bac9bf50003983e6773628fd2a4b0
John Godley <johng75@gmail.com>
2022-06-30 13:41:51 +0100

    Get working on WP 6 with Gutenberg 13.5.2
    It needs a bit of work, but at least there are no errors

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
-
-module.exports = {
-	...defaultConfig,
-	resolve: {
-		...defaultConfig.resolve,
-//		symlinks: false,
-	},
-};

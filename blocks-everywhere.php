1eb06682c28c0ea7929e6fed42bca67f0abf0239
John Godley <johng75@gmail.com>
2022-10-17 14:04:34 +0100

    Update to 1.6.0

--- blocks-everywhere.php
+++ blocks-everywhere.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Blocks Everywhere
 Description: Because somewhere is just not enough. Add Gutenberg to WordPress comments, bbPress forums, and BuddyPress streams. Also enables Gutenberg for comment & bbPress moderation.
-Version: 1.5.0
+Version: 1.6.0
 Author: Automattic
 Text Domain: 'blocks-everywhere'
 */

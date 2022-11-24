50a35344da86c30a5c421f10a91e0af30261f4aa
vasyas <vasyl@stashuk.com>
2022-11-24 12:11:54 +0100

    Fixed image location

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
+// update resource asset to have public path inside plugin
+// required for correctly serving assets in production
+defaultConfig.module.rules
+	.filter( ( r ) => r.type === 'asset/resource' )
+	.forEach( ( r ) => {
+		r.generator.publicPath = '/wp-content/plugins/blocks-everywhere/build/';
+	} );
+
 // Default @wordpress/scripts but output with .min in the filename
 module.exports = {
 	...defaultConfig,

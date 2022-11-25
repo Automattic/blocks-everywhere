1c2529a17a8b9c250481cb14484e6e1ae9fcba88
vasyas <vasyl@stashuk.com>
2022-11-25 14:00:52 +0100

    Support blogs besides WPcom

--- src/support-content-block/block.ts
+++ src/support-content-block/block.ts
@@ -1,7 +1,18 @@
 import { __ } from '@wordpress/i18n';
 
 export const SUPPORT_PAGE_PATTERN = /^https?:\/\/wordpress\.com\/((?<lang>[a-z]{2})\/)?support\/(?<slug>\S+)$/i;
-export const FORUM_TOPIC_PATTERN = /^https?:\/\/wordpress\.com\/((?<lang>[a-z]{2})\/)?forums\/topic\/(?<slug>\S+)$/i;
+/*
+Forum Pattern cases
+
+https://wordpress.com/forums/topic/ecommerce-for-1-month/?view=all
+https://wordpress.com/es/forums/topic/planes-47/?view=all
+https://test.forums.wordpress.com/topic/topic-7/edit/?view=all
+
+https://wordpress.org/support/topic/before-posting-in-everything-wordpress/
+https://bbpress.org/forums/topic/bbpress-redundant-h1-tag/
+ */
+export const FORUM_TOPIC_PATTERN =
+	/^https?:\/\/(?<domain>[-a-zA-Z0-9@:%._+~#=]*)\/((?<lang>[a-z]{2})\/)?(?<prefix>[-a-zA-Z0-9@:%._+~#=\/]*)topic\/(?<slug>\S+)$/i;
 const EMBED_CONTENT_MAXLENGTH = 400;
 const AVERAGE_READING_SPEED = 250; // words per minute
 
@@ -80,11 +91,11 @@ export async function fetchSupportPageAttributes( url: string ): Promise< Suppor
  * Fetch forum topic via API and parse its data into block attributes
  */
 export async function fetchForumTopicAttributes( url: string ): Promise< SupportContentBlockAttributes > {
-	// TODO Support non WPCOM forums
-
 	const { blog, slug } = getForumTopicSlugFromUrl( url );
 
-	const apiUrl = `https://public-api.wordpress.com/wp/v2/sites/${ blog }/topic?slug=${ encodeURIComponent( slug ) }`;
+	const apiUrl = blog.endsWith( 'wordpress.com' )
+		? `https://public-api.wordpress.com/wp/v2/sites/${ blog }/topic?slug=${ encodeURIComponent( slug ) }`
+		: `https://${ blog }/wp-json/wp/v2/topic?slug=${ encodeURIComponent( slug ) }`;
 	const response = await fetch( apiUrl );
 
 	if ( ! response.ok ) {
@@ -142,10 +153,10 @@ function getForumTopicSlugFromUrl( url: string ): { blog: string; slug: string }
 		slug = slug.slice( 0, slug.indexOf( '/' ) );
 	}
 
-	return {
-		blog: `${ lang }.forums.wordpress.com`,
-		slug,
-	};
+	const blog =
+		urlMatches.groups.domain == 'wordpress.com' ? `${ lang }.forums.wordpress.com` : urlMatches.groups.domain;
+
+	return { blog, slug };
 }
 
 function stripHtml( html: string ): string {

73141c633c553f05aac0a3e824f65121548f2e5a
John <johng75@gmail.com>
2022-12-05 10:03:06 +0000

    Fix is-pressed style (#64)
    * Apply lintfix to blocks
    
    * Add list item to allowed blocks
    
    * Fix toggle buttons turning black
    
    * Fix bad filter in readme

--- bin/lintfix.sh
+++ bin/lintfix.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 sed "s/<?php /<?php\n\/\/ phpcs:ignore\n/" build/index.min.asset.php | sed "s/);/);\n/" > build/index.min.asset.php.new
 mv build/index.min.asset.php.new build/index.min.asset.php
+
+sed "s/<?php /<?php\n\/\/ phpcs:ignore\n/" build/support-content-editor.min.asset.php | sed "s/);/);\n/" > build/support-content-editor.min.asset.php.new
+mv build/support-content-editor.min.asset.php.new build/support-content-editor.min.asset.php
+
+sed "s/<?php /<?php\n\/\/ phpcs:ignore\n/" build/support-content-view.min.asset.php | sed "s/);/);\n/" > build/support-content-view.min.asset.php.new
+mv build/support-content-view.min.asset.php.new build/support-content-view.min.asset.php

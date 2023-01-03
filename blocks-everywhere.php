8d938e60e46f5c7428a933117a07c7da2443c4d9
John <johng75@gmail.com>
2023-01-03 14:28:32 +0000

    Bump to 1.14.0 (#131)

--- blocks-everywhere.php
+++ blocks-everywhere.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Blocks Everywhere
 Description: Because somewhere is just not enough. Add Gutenberg to WordPress comments, bbPress forums, and BuddyPress streams. Also enables Gutenberg for comment & bbPress moderation.
-Version: 1.13.4
+Version: 1.14.0
 Author: Automattic
 Text Domain: 'blocks-everywhere'
 */

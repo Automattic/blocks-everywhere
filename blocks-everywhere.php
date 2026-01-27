c8370b4269dadf9408588303b676cb3f73b13ca6
Chris Huber <chubes4@users.noreply.github.com>
2026-01-27 17:48:26 -0600

    release: v1.25.5

--- blocks-everywhere.php
+++ blocks-everywhere.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Blocks Everywhere
 Description: Because somewhere is just not enough. Add Gutenberg to WordPress comments, bbPress forums, and BuddyPress streams. Also enables Gutenberg for comment & bbPress moderation.
-Version: 1.25.4
+Version: 1.25.5
 Author: Automattic
 Text Domain: 'blocks-everywhere'
 */
--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -2,6 +2,11 @@
 
 All notable changes to Blocks Everywhere are documented in this file.
 
+## [1.25.5] - 2026-01-27
+
+### Changed
+- Add ThemeSupportsDispatcher for responsive embed classes
+
 ## [1.25.4] - 2026-01-27
 
 ### Fixed
